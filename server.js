2e2c13c843a86a98391730a8cdaa526eb0fae9a3
esterssc24 <ester.ssc@hotmail.com>
2024-12-15 18:29:09 -0300

    ALTERAÇÃO CONTEUDO

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Bem-vindo ao Servidor Node.js!');
+    res.send('ACABAAAAAAAAAA LOGO PELO AMOR DE DEUS ! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH');
 });
 
 app.listen(3000, () => {
